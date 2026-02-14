1805d3cc94a9c26638cce1023c9851aeef274d7e
ahmeekhan221-blip <ahmeekhan221@gmail.com>
2026-02-14 17:47:12 +0500

    final

--- components/About.tsx
+++ components/About.tsx
@@ -658,7 +658,7 @@ const AboutUs = () => {
     offset: ["start end", "end start"]
   });
 
-  const opacity = useTransform(scrollYProgress, [0, 0.2, 0.8, 1], [0, 1, 1, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 0, 0, 1], [0, 1, 1, 0]);
   const scale = useTransform(scrollYProgress, [0, 0.2, 0.8, 1], [0.8, 1, 1, 0.8]);
 
   return (
